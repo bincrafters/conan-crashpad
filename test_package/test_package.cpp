1cb57d2f7613320ce61faaa6703bdb6d9835f60b
René Meusel <rene.meusel@nexenio.com>
2019-09-03 09:13:59 +0200

    add a simple test_package

---
+++ test_package/test_package.cpp
@@ -0,0 +1,12 @@
+#include <map>
+#include <string>
+#include <vector>
+
+#include "client/crashpad_client.h"
+#include "client/settings.h"
+
+int main() {
+    crashpad::CrashpadClient client;
+
+    return 0;
+}
