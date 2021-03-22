bincrafters/conan-crashpad

commit 614aef32adddc2eabc7a3b6c6a3c888a86bb5f3f

Update Conan conventions
Automatically created by bincrafters-conventions 0.30.2

conanfile.py
@@ -24,7 +24,7 @@ class CrashpadConan(ConanFile):
     _build_dir = os.path.join(_source_dir, _build_name)
 
     def build_requirements(self):
-        self.build_requires("depot_tools_installer/20200515@bincrafters/stable")
+        self.build_requires("depot_tools/cci.20201009")
         self.build_requires("ninja/1.10.2")
 
     def _mangle_spec_for_gclient(self, solutions):

commit 74fb528e0237ad83c076a9a7999b408c251303b9

Switch back to bincrafters' depot_tools_installer
The conan-center-index one is broken on Windows, switching to that in 614aef32adddc2eabc7a3b6c6a3c888a86bb5f3f broke the Windows builds.

conanfile.py
@@ -25,7 +25,7 @@ class CrashpadConan(ConanFile):
     _build_dir = os.path.join(_source_dir, _build_name)
 
     def build_requirements(self):
-        self.build_requires("depot_tools/cci.20201009")
+        self.build_requires("depot_tools_installer/20200515@bincrafters/stable")
         self.build_requires("ninja/1.10.2")
 
     def _mangle_spec_for_gclient(self, solutions):